ec3faf6584961b8bacc09f7042df559d69e79925
gabrmanh <gmanhani@yahoo.com>
2025-06-20 18:29:22 -0300

    feat: implement web socket server

---
+++ src/config/websocketServer.ts
@@ -0,0 +1,29 @@
+import WebSocket, { WebSocketServer } from 'ws';
+import { handleMessage } from '../controllers/messageController';
+
+export function startWebSocketServer(): void {
+  const wss = new WebSocketServer({ port: 8080 });
+
+  wss.on('connection', (ws: WebSocket) => {
+    console.log('connected');
+
+    ws.on('message', (message: string | Buffer) => {
+      const msg = message.toString();
+      console.log('received:', msg);
+
+      const response = handleMessage(msg);
+
+      wss.clients.forEach((client) => {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(response);
+        }
+      });
+    });
+
+    ws.on('close', () => {
+      console.log('disconnected');
+    });
+  });
+
+  console.log('running on ws://localhost:8080');
+}
